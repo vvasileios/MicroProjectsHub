e7f1bd8a8d40a4df66a3b83e808a18f1f529c5d9
Vasileios <vtvasileiadis@gmail.com>
2024-03-19 18:50:59 +0200

    redo digital clock

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -1,25 +1,40 @@
-function updateClock() {
-  const today = new Date();
-  const hour = today.getHours().toString();
-  const minutes = today.getMinutes().toString().padStart(2, "0");
-  const seconds = today.getSeconds().toString().padStart(2, "0");
-  const date = today.toDateString();
+setInterval(() => {
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
+  let meridiem = document.getElementById("meridiem");
 
-  if (document.getElementById("hour").innerText !== hour) {
-    document.getElementById("hour").innerText = hour;
+  let hh = document.getElementById("hh");
+  let mm = document.getElementById("mm");
+  let ss = document.getElementById("ss");
+
+  let dotHours = document.querySelector(".h_dot");
+  let dotMinutes = document.querySelector(".m_dot");
+  let dotSeconds = document.querySelector(".s_dot");
+
+  let newHours = new Date().getHours();
+  let newMinutes = new Date().getMinutes();
+  let newSeconds = new Date().getSeconds();
+  let system = newHours >= 12 ? "PM" : "AM";
+
+  if (newHours > 12) {
+    newHours = newHours - 12;
   }
 
-  if (document.getElementById("minute").innerText !== minutes) {
-    document.getElementById("minute").innerText = minutes;
-  }
+  newHours = newHours < 10 ? "0" + newHours : newHours;
+  newMinutes = newMinutes < 10 ? "0" + newMinutes : newMinutes;
+  newSeconds = newSeconds < 10 ? "0" + newSeconds : newSeconds;
 
-  if (document.getElementById("second").innerText !== seconds) {
-    document.getElementById("second").innerText = seconds;
-  }
+  hours.innerHTML = newHours;
+  minutes.innerHTML = newMinutes;
+  seconds.innerHTML = newSeconds;
+  meridiem.innerHTML = system;
 
-  if (document.getElementById("date").innerText !== date) {
-    document.getElementById("date").innerText = date;
-  }
-}
+  hh.style.strokeDashoffset = 440 - (440 * newHours) / 12;
+  mm.style.strokeDashoffset = 440 - (440 * newMinutes) / 60;
+  ss.style.strokeDashoffset = 440 - (440 * newSeconds) / 60;
 
-setInterval(updateClock, 1000);
+  dotHours.style.transform = `rotate(${newHours * 30}deg)`;
+  dotMinutes.style.transform = `rotate(${newMinutes * 6}deg)`;
+  dotSeconds.style.transform = `rotate(${newSeconds * 6}deg)`;
+}, 1000);
