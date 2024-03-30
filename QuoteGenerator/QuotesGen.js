e585be9a65f30208ac91371385a64310353e8e03
Vasileios <vtvasileiadis@gmail.com>
2024-03-30 12:05:29 +0200

    init quote generator

---
+++ QuoteGenerator/QuotesGen.js
@@ -0,0 +1,127 @@
+const localQuotes = [
+  {
+    text: "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    author: "Thomas Edison",
+    category: "Inspiration",
+  },
+  {
+    text: "You can observe a lot just by watching.",
+    author: "Yogi Berra",
+    category: "Wisdom",
+  },
+  {
+    text: "A house divided against itself cannot stand.",
+    author: "Abraham Lincoln",
+    category: "Unity",
+  },
+  {
+    text: "Difficulties increase the nearer we get to the goal.",
+    author: "Johann Wolfgang von Goethe",
+    category: "Perseverance",
+  },
+  {
+    text: "Fate is in your hands and no one elses",
+    author: "Byron Pulsifer",
+    category: "Destiny",
+  },
+  {
+    text: "Be the chief but never the lord.",
+    author: "Lao Tzu",
+    category: "Leadership",
+  },
+  {
+    text: "Nothing happens unless first we dream.",
+    author: "Carl Sandburg",
+    category: "Dreams",
+  },
+  {
+    text: "Well begun is half done.",
+    author: "Aristotle",
+    category: "Action",
+  },
+  {
+    text: "Life is a learning experience, only if you learn.",
+    author: "Yogi Berra",
+    category: "Learning",
+  },
+  {
+    text: "Self-complacency is fatal to progress.",
+    author: "Margaret Sangster",
+    category: "Self-improvement",
+  },
+  {
+    text: "Peace comes from within. Do not seek it without.",
+    author: "Buddha",
+    category: "Inner Peace",
+  },
+  {
+    text: "What you give is what you get.",
+    author: "Byron Pulsifer",
+    category: "Karma",
+  },
+  {
+    text: "We can only learn to love by loving.",
+    author: "Iris Murdoch",
+    category: "Love",
+  },
+  {
+    text: "Life is change. Growth is optional. Choose wisely.",
+    author: "Karen Clark",
+    category: "Growth",
+  },
+  {
+    text: "You'll see it when you believe it.",
+    author: "Wayne Dyer",
+    category: "Belief",
+  },
+  {
+    text: "Today is the tomorrow we worried about yesterday.",
+    author: null,
+    category: "Anxiety",
+  },
+  {
+    text: "It's easier to see the mistakes on someone else's paper.",
+    author: null,
+    category: "Perspective",
+  },
+  {
+    text: "Every man dies. Not every man really lives.",
+    author: null,
+    category: "Living",
+  },
+  {
+    text: "To lead people walk behind them.",
+    author: "Lao Tzu",
+    category: "Leadership",
+  },
+  {
+    text: "Having nothing, nothing can he lose.",
+    author: "William Shakespeare",
+    category: "Contentment",
+  },
+  {
+    text: "Trouble is only opportunity in work clothes.",
+    author: "Henry J. Kaiser",
+    category: "Opportunity",
+  },
+  {
+    text: "A rolling stone gathers no moss.",
+    author: "Publilius Syrus",
+    category: "Growth",
+  },
+  {
+    text: "Ideas are the beginning points of all fortunes.",
+    author: "Napoleon Hill",
+    category: "Creativity",
+  },
+  {
+    text: "Everything in life is luck.",
+    author: "Donald Trump",
+    category: "Luck",
+  },
+  {
+    text: "Doing nothing is better than being busy doing nothing.",
+    author: "Lao Tzu",
+    category: "Productivity",
+  },
+];
---
+++ QuoteGenerator/script.js
@@ -0,0 +1,53 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+// function loading() {
+//   loader.hidden = false;
+//   quoteContainer.hidden = true;
+// }
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+function newQuote() {
+  loading();
+
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+async function getQuotes() {
+  loading();
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+
+getQuotes();
